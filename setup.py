51e7b20c8e5449826f1f46d896e26b68d78d31e6
bsungwoo <61150422+bsungwoo@users.noreply.github.com>
2022-05-04 01:46:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description = "Cell type inference by domain adaptation of single-cell and spatial transcriptomic data",
     url = "https://github.com/mexchy1000/CellDART.git",
     author = "Hongyoon Choi, Sungwoo Bae",
-    packages=find_packages('CellDART'),
+    packages=find_packages(include=['CellDART', 'CellDART.*']),
     install_requires = ["scanpy==1.5.1","pandas==1.3.5","numpy==1.21.6",
                         "h5py==2.10.0", "jupyter",
                         "keras==2.3.1", "tensorflow==1.14.0", "tensorflow-gpu==1.14.0"]
