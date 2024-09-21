3922083e3c5df9dfb59ca0d39ed45641deb300db
bsungwoo <bsungwoo@naver.com>
2024-09-21 20:20:53 +0900

    Update setup.py
    Fix bug in version: collision

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url = "https://github.com/mexchy1000/CellDART.git",
     author = "Hongyoon Choi, Sungwoo Bae",
     packages=find_packages(include=['CellDART', 'CellDART.*']),
-    install_requires = ["tensorflow~=2.9","pandas~=1.4","numpy~=1.20",
+    install_requires = ["tensorflow~=2.9","pandas","numpy",
                         "scanpy","leidenalg","igraph",
                         "jupyter","ply","pytest"]
 )
