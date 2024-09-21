e7c1f3b836ebc4aab76c35de9c665f9a8f42a1a2
bsungwoo <bsungwoo@naver.com>
2024-09-21 20:19:45 +0900

    Update setup.py
    Fix bug

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name = "CellDART",
-    version = "0.1.2",
+    version = "0.1.3",
     description = "Cell type inference by domain adaptation of single-cell and spatial transcriptomic data",
     url = "https://github.com/mexchy1000/CellDART.git",
     author = "Hongyoon Choi, Sungwoo Bae",
     packages=find_packages(include=['CellDART', 'CellDART.*']),
-    install_requires = ["tensorflow~=2.9","tensorflow-gpu~=2.9", 
-                        "pandas~=1.4","numpy~=1.20",
+    install_requires = ["tensorflow~=2.9","pandas~=1.4","numpy~=1.20",
                         "scanpy","leidenalg","igraph",
                         "jupyter","ply","pytest"]
 )
